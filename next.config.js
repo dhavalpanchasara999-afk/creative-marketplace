dd20596b85def422776387315265e7ff70ed545f
dhavalpanchasara999-afk <dhavalpanchasara999@gmail.com>
2026-06-04 22:39:11 +0530

    to fix vercel.json error

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/index.html',
+      },
+      {
+        source: '/marathi-fonts',
+        destination: '/marathi-fonts.html',
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig;
